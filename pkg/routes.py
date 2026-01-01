97786fc69a17844c53d30f9d6c07ff3aac8393c0
Kolad3 <koladeemmanuela32@gmail.com>
2026-01-01 08:53:04 +0100

    created the pages for the navigation pane and successfully linked them to the backend routes while utilizing the template structure

--- pkg/routes.py
+++ pkg/routes.py
@@ -4,3 +4,15 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/team/')
+def team():
+    return render_template('team.html')
+
+@app.route('/testimonials/')
+def testimonials():
+    return render_template('testimonials.html')
