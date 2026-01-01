8f3e234f1bf9d5481e2f16804f6436a39fec1558
Kolad3 <koladeemmanuela32@gmail.com>
2026-01-01 15:00:59 +0100

    created more pages to navigate from the nav bar to and created the respective routes

--- pkg/routes.py
+++ pkg/routes.py
@@ -16,3 +16,39 @@ def team():
 @app.route('/testimonials/')
 def testimonials():
     return render_template('testimonials.html')
+
+@app.route('/faq/')
+def faq():  
+    return render_template('faq.html')
+
+@app.route('/pricing/')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/projects/all/')
+def projects_all():
+    return render_template('projects.html')
+
+@app.route('/projects/single/')
+def projects_single():
+    return render_template('projects-single.html')
+
+@app.route('/services/single/')
+def service_single():
+    return render_template('service-single.html')   
+
+@app.route('/services/')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact/')
+def contact():  
+    return render_template('contact.html')
+
+@app.route('/news/right/sidebar/')
+def news_right_sidebar():
+    return render_template('news-right-sidebar.html')
+
+@app.route('/news/single/')
+def news_single():
+    return render_template('news-single.html')
